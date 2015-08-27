ad500a2b22abbd79edbb27c6c730d81d3a935857
Julian Arnold <julian.arnold@rwth-aachen.de>
2015-08-27 16:51:18 +0200

    preamble: bipolar generation

--- tools/generate_preamble.m
+++ tools/generate_preamble.m
@@ -1,4 +1,4 @@
-hpn = comm.PNSequence('Polynomial',[8 7 2 1 0], 'InitialConditions',[1 1 1 1 1 1 1 1 ], 'SamplesPerFrame', 128);
+hpn = comm.PNSequence('Polynomial',[8 7 2 1 0], 'InitialConditions',[1 1 1 1 1 1 1 1 ], 'SamplesPerFrame', 256);
 preamble = step(hpn);
 
 figure;
@@ -9,9 +9,12 @@ preamble_diff = [];
 
 for ii = 1:length(preamble)
     if(ii == 1)
-        preamble_diff(ii) = preamble(ii);
+        val = (preamble(ii)-0.5)*2;
+        preamble_diff(ii) = val;
     else
-        preamble_diff(ii) = preamble(ii) * preamble(ii-1);
+        val = (preamble(ii)-0.5)*2;
+        val2 = (preamble(ii-1)-0.5)*2;
+        preamble_diff(ii) = val * val2;
     end
 end
 
@@ -21,5 +24,33 @@ figure;
 c = xcorr(preamble_diff, preamble_diff);
 plot(c);
 
-csvwrite('ml_preamble_128.txt', transpose(preamble));
-csvwrite('diff_ml_preamble_128.txt', preamble_diff);
+csvwrite('ml_preamble_256.txt', transpose(preamble));
+csvwrite('diff_ml_preamble_256.txt', preamble_diff);
+
+mod = [];
+index = 1;
+for ii = 1:2:length(preamble_diff)
+    val = preamble_diff(ii) + 2 * preamble_diff(ii+1);
+    if(val == 0)
+        mod(index) = 1+0i; 
+    end
+    
+    if(val == 1)
+        mod(index) = 0+1i;
+    end
+    
+    if(val == 2)
+        mod(index) = -1+0i;
+    end
+    
+    if(val == 3)
+        mod(index) = 0-1i;
+    end 
+    index = index + 1;
+end
+
+figure;
+c = xcorr(mod, mod);
+%plot(real(mod),imag(mod));
+plot(abs(c));
+
