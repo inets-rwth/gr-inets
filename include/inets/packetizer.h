35379ad687ab14c58e7cdaab242776ef5ae7d5c1
inets <julian@elitecoding.org>
2015-09-01 12:30:11 +0200

    packetizer: rework

--- include/inets/packetizer.h
+++ include/inets/packetizer.h
@@ -46,7 +46,7 @@ namespace gr {
        * class. inets::packetizer::make is the public interface for
        * creating new instances.
        */
-      static sptr make();
+      static sptr make(const std::vector<unsigned char> &preamble, int padding);
     };
 
   } // namespace inets
