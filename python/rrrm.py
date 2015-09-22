8fa6e882a13ca6cd0e7fcc8ccb39b50ae1380991
inets <julian@elitecoding.org>
2015-09-22 10:17:27 +0200

    rrrm and sow rework

--- python/rrrm.py
+++ python/rrrm.py
@@ -1,29 +1,30 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# 
+#
 # Copyright 2015 <+YOU OR YOUR COMPANY+>.
-# 
+#
 # This is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 3, or (at your option)
 # any later version.
-# 
+#
 # This software is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this software; see the file COPYING.  If not, write to
 # the Free Software Foundation, Inc., 51 Franklin Street,
 # Boston, MA 02110-1301, USA.
-# 
+#
 
 import string
 import numpy
 import time
 import Queue
 from gnuradio import gr
+from gnuradio import digital
 import pmt
 import threading
 import control
@@ -41,13 +42,12 @@ class rrrm(gr.basic_block):
     PACKET_TYPE_SWITCH_ACCEPT = 2
     PACKET_TYPE_SWITCH_REJECT = 3
 
-    def __init__(self, channel_map):
+    def __init__(self, node_id, channel_map):
         gr.basic_block.__init__(self,
             name="rrrm",
             in_sig=[],
             out_sig=[])
 
-        self.channel_map = channel_map
         self.message_port_register_in(pmt.intern('payload_in'))
         self.message_port_register_out(pmt.intern('payload_out'))
         self.message_port_register_in(pmt.intern('radar_in'))
@@ -57,7 +57,10 @@ class rrrm(gr.basic_block):
         self.set_msg_handler(pmt.intern('payload_in'), self.handle_payload_message)
         self.set_msg_handler(pmt.intern('rrrm_in'), self.handle_rrrm_message)
         self.set_msg_handler(pmt.intern('radar_in'), self.handle_radar_message)
-        self.app_queue = Queue.Queue()
+
+        self.channel_map = channel_map
+        self.node_id = node_id
+
         self.state = self.STATE_FORWARD_PAYLOAD
         self.curr_channel_id = 0
         self.next_channel_id = 0
@@ -65,38 +68,39 @@ class rrrm(gr.basic_block):
         self.thread_lock = threading.Lock()
         self.switch_ack_thread = None
         self.switch_ack_received = False
-        
+
         try:
-	    self.antenna_control = control.control("/dev/ttyACM0")
+            self.antenna_control = control.control("/dev/ttyACM0")
             self.antenna_control.open()
         except:
             print 'could not open serial port'
- 
-    def handle_payload_message(self, msg_pmt):  
+
+    def handle_payload_message(self, msg_pmt):
         with self.thread_lock:
             if self.state == self.STATE_FORWARD_PAYLOAD:
-                print 'RRRM: App message. Run state. Forwarding message'
+                print 'RRRM: payload_in. Run state. Forwarding message'
                 self.send_data_message(msg_pmt)
-        
+
             if self.state == self.STATE_SWITCH:
-                print 'RRRM: App message. Switch state. Buffering message'
-                self.app_queue.put(msg_pmt)
-    
+                print 'RRRM: payload_in. Switch state. Discarding message'
+
     def handle_radar_message(self, msg_pmt):
         with self.thread_lock:
-            
+            print 'RRRM: radar_in'
+
             if self.state == self.STATE_SWITCH:
+                print 'RRRM: WARN: Already in SWITCH state.'
                 return
 
             self.state = self.STATE_SWITCH
-            
+
             #calculate new path
             if self.curr_channel_id == 0:
-                self.next_channel_id = 1 
-            else: 
+                self.next_channel_id = 1
+            else:
                 self.next_channel_id = 0
 
-            print ('RRRM: Radar. Curr chan = '+
+            print ('RRRM: Curr chan = '+
                 str(self.curr_channel_id)+" next chan = "+str(self.next_channel_id))
 
             self.next_channel_pos = self.channel_map[self.next_channel_id]
@@ -111,36 +115,55 @@ class rrrm(gr.basic_block):
             self.send_switch_command(self.next_channel_id)
             time.sleep(0.1)
             count += 1
+        print 'RRRM: FATAL: Missing SWITCH ACK'
+
+    def build_link_layer_packet(self, msg_str):
+        msg_str = chr(self.node_id) + msg_str
+        msg_str = digital.crc.gen_and_append_crc32(msg_str)
+        #packet_str_total = digital.packet_utils.whiten(packet_str_total, 0)
+        send_pmt = self.get_pmt_from_data_str(msg_str)
+        return send_pmt
 
     def send_switch_command(self, new_channel_id):
-        packet_str = chr(self.PACKET_TYPE_SWITCH)
-        packet_str = packet_str + chr(new_channel_id)
-        send_pmt = self.get_pmt_from_data_str(packet_str)  
-        self.message_port_pub(pmt.intern('rrrm_out'), send_pmt) 
+        msg_str = chr(self.PACKET_TYPE_SWITCH)
+        msg_str = msg_str + chr(new_channel_id)
+        send_pmt = self.build_link_layer_packet(msg_str)
+        self.message_port_pub(pmt.intern('rrrm_out'), send_pmt)
 
     def send_switch_accept(self):
-        packet_str = chr(self.PACKET_TYPE_SWITCH_ACCEPT)
-        send_pmt = self.get_pmt_from_data_str(packet_str)  
-        self.message_port_pub(pmt.intern('rrrm_out'), send_pmt) 
+        msg_str = chr(self.PACKET_TYPE_SWITCH_ACCEPT)
+        send_pmt = self.build_link_layer_packet(msg_str)
+        self.message_port_pub(pmt.intern('rrrm_out'), send_pmt)
 
     def send_switch_reject(self):
-        packet_str = chr(self.PACKET_TYPE_SWITCH_REJECT)
-        send_pmt = self.get_pmt_from_data_str(packet_str)  
-        self.message_port_pub(pmt.intern('rrrm_out'), send_pmt) 
+        msg_str = chr(self.PACKET_TYPE_SWITCH_REJECT)
+        send_pmt = self.build_link_layer_packet(msg_str)
+        self.message_port_pub(pmt.intern('rrrm_out'), send_pmt)
 
-    def send_data_message(self, msg_pmt): 
+    def send_data_message(self, msg_pmt):
         msg_str = self.get_data_str_from_pmt(msg_pmt)
         msg_str = chr(self.PACKET_TYPE_DATA) + msg_str
-        send_pmt = self.get_pmt_from_data_str(msg_str)
+        send_pmt = self.build_link_layer_packet(msg_str)
         self.message_port_pub(pmt.intern('rrrm_out'), send_pmt)
 
     def handle_rrrm_message(self, msg_pmt):
         with self.thread_lock:
+            print 'RRRM: rrrm_in'
+
             msg_str = self.get_data_str_from_pmt(msg_pmt)
-            msg_type, msg_data = self.parse_rrrm_message(msg_str)
+            ok, node_id, msg_type, msg_data = self.parse_rrrm_message(msg_str)
+
+            if not ok:
+                print 'RRRM: Bad CRC'
+                return
+
+            print 'RRRM: Message: node_id = ' + str(node_id) + ' type = ' + str(msg_type)
+
+            if node_id == self.node_id:
+                print 'RRRM: discarding... '
+                return
 
             if msg_type == self.PACKET_TYPE_DATA:
-                print 'RRRM: PAYLOAD'
                 send_pmt = self.get_pmt_from_data_str(msg_data)
                 self.message_port_pub(pmt.intern('payload_out'), send_pmt)
 
@@ -153,7 +176,7 @@ class rrrm(gr.basic_block):
                     self.antenna_control.move_to(self.next_channel_pos)
 
                 self.curr_channel_id = self.next_channel_id
-                
+
                 #make sure switch ack reaches other side before turning antenna
                 self.send_switch_accept()
                 time.sleep(0.01)
@@ -164,7 +187,6 @@ class rrrm(gr.basic_block):
                 self.state = self.STATE_FORWARD_PAYLOAD
 
             if msg_type == self.PACKET_TYPE_SWITCH_ACCEPT:
-                print 'RRRM: Switch ACK'
 
                 if self.switch_ack_received == True: #Duplicate ACK, we'll receive 3 ACKs
                     return
@@ -181,10 +203,18 @@ class rrrm(gr.basic_block):
 
                 self.state = self.STATE_FORWARD_PAYLOAD
 
-    def parse_rrrm_message(self, message_str):
-        message_type = ord(message_str[0])
-        message_data = message_str[1:]
-        return (message_type, message_data)        
+    def parse_link_layer_packet(self, msg_str):
+        (ok, msg_str) = digital.crc.check_crc32(msg_str)
+        return (ok, msg_str)
+
+    def parse_rrrm_message(self, msg_str):
+        ok, msg_str = self.parse_link_layer_packet(msg_str)
+        if not ok:
+            return (False, None, None, None)
+        node_id = ord(msg_str[0])
+        msg_type = ord(msg_str[1])
+        msg_str = msg_str[2:]
+        return (True, node_id, msg_type, msg_str)
 
     ################ PMT helper functions #####################
     def get_data_str_from_pmt(self, msg_pmt):
@@ -197,7 +227,7 @@ class rrrm(gr.basic_block):
         send_pmt = pmt.make_u8vector(len(msg_str), ord(' '))
         for i in range(len(msg_str)):
             pmt.u8vector_set(send_pmt, i, ord(msg_str[i]))
-        
+
         send_pmt = pmt.cons(pmt.PMT_NIL, send_pmt)
         return send_pmt
 
--- python/stop_and_wait_arq.py
+++ python/stop_and_wait_arq.py
@@ -1,23 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# 
+#
 # Copyright 2015 <+YOU OR YOUR COMPANY+>.
-# 
+#
 # This is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 3, or (at your option)
 # any later version.
-# 
+#
 # This software is distributed in the hope that it will be useful,
     # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this software; see the file COPYING.  If not, write to
 # the Free Software Foundation, Inc., 51 Franklin Street,
 # Boston, MA 02110-1301, USA.
-# 
+#
 
 import string
 import csv
@@ -38,12 +38,12 @@ class stop_and_wait_arq(gr.basic_block):
     PACKET_TYPE_DATA = 0
     PACKET_TYPE_ACK = 1
 
-    def __init__(self, node_id, ack_timeout, max_mtu_size, use_ack, stat_update_interval):
+    def __init__(self, ack_timeout, max_mtu_size, use_ack, stat_update_interval):
         gr.basic_block.__init__(self,
             name="stop_and_wait_arq",
             in_sig=[],
             out_sig=[])
-	
+
         self.message_port_register_in(pmt.intern('from_app'))
         self.message_port_register_in(pmt.intern('snr'))
         self.message_port_register_out(pmt.intern('to_app'))
@@ -52,19 +52,18 @@ class stop_and_wait_arq(gr.basic_block):
         self.set_msg_handler(pmt.intern('from_app'), self.handle_app_message)
         self.set_msg_handler(pmt.intern('from_phy'), self.handle_phy_message)
         self.set_msg_handler(pmt.intern('snr'), self.handle_snr_message)
-        self.app_queue = Queue.Queue() 
+        self.app_queue = Queue.Queue()
         self.state = self.STATE_IDLE
         self.last_tx_time = 0
         self.last_tx_packet = 0
         self.ack_timeout = ack_timeout #in fractional seconds
         self.max_mtu_size = max_mtu_size
-    	self.use_ack = use_ack
+        self.use_ack = use_ack
         self.wait_for_frag = False
         self.packet_buffer = []
         self.last_frag_index = 0
         self.tx_seq_num = 0
-        self.thread_lock = threading.Lock()        
-        self.node_id = node_id        
+        self.thread_lock = threading.Lock()
 
         #statistics
         self.total_num_rx_packets = 0
@@ -77,7 +76,7 @@ class stop_and_wait_arq(gr.basic_block):
         self.rx_statistics_update_interval = stat_update_interval
         numpy.random.seed(0)
         self.test_data = numpy.random.randint(0, 256, 500)
-        
+
         self.csv_fields = ['Date', 'SNR', '#BER / Bad Packet', 'BER', 'PER']
         curr_time = time.strftime("%d.%m.%Y-%H-%M-%S")
         self.log_file_name = '/home/inets/stop_and_wait_arq_log_'+curr_time+'.csv'
@@ -87,30 +86,27 @@ class stop_and_wait_arq(gr.basic_block):
 
     def handle_snr_message(self, msg):
         snr_pmt = pmt.to_python(msg)
-        self.snr_log.append(float(snr_pmt)) 
+        self.snr_log.append(float(snr_pmt))
 
     def handle_app_message(self, msg_pmt):
         #print 'app interrupt'
         with self.thread_lock:
           packets_str = self.fragment_packet(msg_pmt)
           for packet_str in packets_str:
-            packet_str_total = chr(self.node_id)
-            packet_str_total += chr(self.tx_seq_num)
-            
+            packet_str_total = chr(self.tx_seq_num)
+
             if self.tx_seq_num < 255:
               self.tx_seq_num = self.tx_seq_num + 1
             else:
               self.tx_seq_num = 0
 
-            packet_str_total += chr(self.PACKET_TYPE_DATA) 
-            packet_str_total += packet_str 
-            packet_str_total = digital.crc.gen_and_append_crc32(packet_str_total)
-            #packet_str_total = digital.packet_utils.whiten(packet_str_total, 0)
-            
+            packet_str_total += chr(self.PACKET_TYPE_DATA)
+            packet_str_total += packet_str
+
             send_pmt = pmt.make_u8vector(len(packet_str_total), ord(' '))
             for i in range(len(packet_str_total)):
               pmt.u8vector_set(send_pmt, i, ord(packet_str_total[i]))
-            
+
             send_pmt = pmt.cons(pmt.PMT_NIL, send_pmt)
             self.app_queue.put(send_pmt)
             self.handle_queue()
@@ -118,63 +114,48 @@ class stop_and_wait_arq(gr.basic_block):
     def handle_queue(self):
         if self.state == self.STATE_IDLE:
           if self.app_queue.empty() == False:
-            print 'Sending packet. Queue fill level = ', self.app_queue.qsize()
+            print 'SAW: Sending packet. Queue fill level = ', self.app_queue.qsize()
             self.last_tx_packet = self.app_queue.get()
             self.last_tx_time = time.time()
-            
+
             if self.use_ack:
-	      self.state = self.STATE_WAIT_FOR_ACK
-            
+                self.state = self.STATE_WAIT_FOR_ACK
+
             self.message_port_pub(pmt.intern('to_phy'), self.last_tx_packet)
-                
+
         elif self.state == self.STATE_WAIT_FOR_ACK:
           #print 'Not sending packet yet. ACK pending'
           if (time.time() - self.last_tx_time) > self.ack_timeout:
-            #retransmit 
-            print 'ACK timeout. Retransmitting'
+            #retransmit
+            print 'SAW: ACK timeout. Retransmitting'
             self.last_tx_time = time.time()
             self.message_port_pub(pmt.intern('to_phy'), self.last_tx_packet)
 
-    def handle_phy_message(self, msg_pmt):        
-        with self.thread_lock:
-          print 'phy interrupt'
+    def handle_phy_message(self, msg_pmt):
+            meta = pmt.to_python(pmt.car(msg_pmt))
+            msg = pmt.cdr(msg_pmt)
+            msg_str = "".join([chr(x) for x in pmt.u8vector_elements(msg)])
 
-          meta = pmt.to_python(pmt.car(msg_pmt))
-          msg = pmt.cdr(msg_pmt)
-          msg_str = "".join([chr(x) for x in pmt.u8vector_elements(msg)])
-         
-          packet_str = msg_str 
-          #packet_str = digital.packet_utils.dewhiten(msg_str, 0)
-          (ok, packet_str) = digital.crc.check_crc32(packet_str)
-          
-          self.update_rx_statistics(packet_str[3:], self.test_data, ok)         
- 
-          if not ok:
-            print '#######################################################'
-            print '################## ERROR: Bad CRC #####################'
-            print '#######################################################'  
-            return
+            packet_str = msg_str
 
-          if ok:
-            packet_node_id_byte = ord(packet_str[0])
-            if packet_node_id_byte == self.node_id:
-                print 'discarding own packet'
-                return
+            self.update_rx_statistics(packet_str[3:], self.test_data, True)
+
             packet_rx_seq_byte = ord(packet_str[1])
             packet_str = packet_str[2:]
             packet_type_byte = ord(packet_str[0])
             packet_str = packet_str[1:]
-            print 'Packet received. node id = ', packet_node_id_byte, ' seq = ', packet_rx_seq_byte, ' type = ', packet_type_byte
-             
+
+            print 'SAW: Packet received. seq = ', packet_rx_seq_byte, ' type = ', packet_type_byte
+
             if packet_type_byte == self.PACKET_TYPE_DATA:
-              if self.use_ack:
-	      	#send ACK packet
-              	print 'Sending ACK'
-              	ack_pdu = self.generate_ack_packet_pdu(packet_rx_seq_byte)
-              	self.message_port_pub(pmt.intern('to_phy'), ack_pdu)
-              
-	      #process fragment
-              self.process_fragment(packet_str)
+                if self.use_ack:
+                    #send ACK packet
+                    print 'SAW: Sending ACK'
+                    ack_pdu = self.generate_ack_packet_pdu(packet_rx_seq_byte)
+                    self.message_port_pub(pmt.intern('to_phy'), ack_pdu)
+
+                #process fragment
+                self.process_fragment(packet_str)
 
             elif packet_type_byte == self.PACKET_TYPE_ACK:
               #mark curr packet as transmitted
@@ -188,7 +169,7 @@ class stop_and_wait_arq(gr.basic_block):
 
         self.total_num_rx_packets += 1
         self.total_num_rx_bits += len(payload_str) * 8
-        
+
         num_bit_errors = 0
         if not crc_ok:
             self.total_num_rx_bad_packets += 1
@@ -205,7 +186,7 @@ class stop_and_wait_arq(gr.basic_block):
             if self.total_num_rx_bad_packets > 0:
                 ber_per_bad_packet = self.total_num_rx_bit_errors / float(self.total_num_rx_bad_packets)
             per = self.total_num_rx_bad_packets / float(self.total_num_rx_packets)
-            
+
             avg_snr = 0
             if self.snr_log > 0:
                 snr_avg_len = 0
@@ -215,7 +196,7 @@ class stop_and_wait_arq(gr.basic_block):
                 avg_snr = avg_snr / snr_avg_len
 
             self.write_rx_statistics(ber_per_bad_packet, ber, per, avg_snr)
-            
+
             self.total_num_rx_packets = 0
             self.total_num_rx_bits = 0
             self.total_num_rx_bit_errors = 0
@@ -226,11 +207,11 @@ class stop_and_wait_arq(gr.basic_block):
     def write_rx_statistics(self, ber_per_bad_packet, ber, per, snr):
         with open(self.log_file_name, 'a') as log_file:
             csv_writer = csv.DictWriter(log_file, fieldnames=self.csv_fields)
-            csv_writer.writerow({'Date' : time.strftime("%d.%m.%Y-%H-%M-%S"), 
-                    'SNR' : "{0:.2f}".format(snr), 
-                    '#BER / Bad Packet' : "{0:.2f}".format(ber_per_bad_packet), 
-                    'BER' : "{0:.8f}".format(ber), 
-                    'PER' : "{0:.8f}".format(per)}) 
+            csv_writer.writerow({'Date' : time.strftime("%d.%m.%Y-%H-%M-%S"),
+                    'SNR' : "{0:.2f}".format(snr),
+                    '#BER / Bad Packet' : "{0:.2f}".format(ber_per_bad_packet),
+                    'BER' : "{0:.8f}".format(ber),
+                    'PER' : "{0:.8f}".format(per)})
 
     def calculate_bit_errors(self, payload_str, test_data):
         data = []
@@ -238,7 +219,7 @@ class stop_and_wait_arq(gr.basic_block):
             data.append(ord(x))
 
         bit_error_count = 0
-        for i in range(0, len(data)):            
+        for i in range(0, len(data)):
             count = 0
             mask = 1
             for j in range(0, 8):
@@ -250,31 +231,27 @@ class stop_and_wait_arq(gr.basic_block):
 
         print '# bit errors = ', bit_error_count
         return bit_error_count
- 
+
     def generate_ack_packet_pdu(self, seq_num):
-        packet_str = chr(self.node_id)
-        packet_str += chr(seq_num)
-        packet_str += chr(self.PACKET_TYPE_ACK) 
-        #add dummy data. Otherwise FEC will make problems
+        packet_str = chr(seq_num)
+        packet_str += chr(self.PACKET_TYPE_ACK)
         packet_str += 'aaa'
-        packet_str = digital.crc.gen_and_append_crc32(packet_str)
-        #packet_str = digital.packet_utils.whiten(packet_str, 0)
-         
+
         send_pmt = pmt.make_u8vector(len(packet_str), ord(' '))
-        
+
         for i in range(len(packet_str)):
           pmt.u8vector_set(send_pmt, i, ord(packet_str[i]))
-        
+
         return pmt.cons(pmt.PMT_NIL, send_pmt)
 
     def process_fragment(self, packet_str):
-        frag_byte = ord(packet_str[0])  
+        frag_byte = ord(packet_str[0])
         frag_index = frag_byte >> 2
         if frag_byte & 0x01 == 1 :
           self.last_frag_index = frag_index
           if frag_byte & 0x02 == 0 :
             self.wait_for_frag = True
-            self.packet_buffer += packet_str[1:] 
+            self.packet_buffer += packet_str[1:]
             return
           else:
             self.wait_for_frag = False
@@ -289,7 +266,7 @@ class stop_and_wait_arq(gr.basic_block):
           for i in range(len(packet_str)):
             pmt.u8vector_set(send_pmt, i, ord(packet_str[i]))
           self.message_port_pub(pmt.intern('to_app'), pmt.cons(pmt.PMT_NIL, send_pmt))
-      
+
     def fragment_packet(self, msg_pmt):
         meta = pmt.to_python(pmt.car(msg_pmt))
         msg = pmt.cdr(msg_pmt)
@@ -302,7 +279,7 @@ class stop_and_wait_arq(gr.basic_block):
         num_mtus = (len(msg_str) // self.max_mtu_size)
         if len(msg_str) % self.max_mtu_size != 0:
           num_mtus = num_mtus + 1
-          
+
         mtu_index_start = 0
         mtu_index_end = 0
         last = False
@@ -313,9 +290,9 @@ class stop_and_wait_arq(gr.basic_block):
 
         if num_mtus > 1:
           frag = True;
-          
+
         for i in range(num_mtus):
-          
+
           if (len(msg_str) - mtu_index_start) > self.max_mtu_size:
             mtu_index_end = mtu_index_start + self.max_mtu_size
           else:
@@ -323,28 +300,28 @@ class stop_and_wait_arq(gr.basic_block):
             last = True
 
           fragment_str = msg_str[mtu_index_start:mtu_index_end]
-         
+
           packet = self.add_frag_hdr(fragment_str, frag, last, i)
-          
+
           mtu_index_start += self.max_mtu_size
 
           # Send the message:
           packet_list.append(packet)
-        
+
         return packet_list
-    
-    def add_frag_hdr(self, payload_str, frag, last_frag, frag_index): 
+
+    def add_frag_hdr(self, payload_str, frag, last_frag, frag_index):
         hdr_str = ''
         if not frag:
           hdr_str = '\x00'
         else:
           frag_byte = 0
           if not last_frag:
-            frag_byte = 1 | frag_index << 2 
+            frag_byte = 1 | frag_index << 2
           else:
             frag_byte = 3 | frag_index << 2
           hdr_str = chr(frag_byte)
-        
+
         packet_str = hdr_str + payload_str
         return packet_str
 
@@ -354,4 +331,4 @@ class stop_and_wait_arq(gr.basic_block):
         data = pmt.u8vector_elements(msg)
         packet_type = data[0]
 
-            
+
