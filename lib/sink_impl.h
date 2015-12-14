1d69e875926bed903fe30b9f195fc8445cfd102d
Christophe <christophe_schmol@yahoo.de>
2015-12-14 16:17:41 +0100

    added sink and source blocks to save/load tagged streams to/from files

---
+++ lib/sink_impl.h
@@ -0,0 +1,100 @@
+/* -*- c++ -*- */
+/* 
+ * Copyright 2015 <+YOU OR YOUR COMPANY+>.
+ * 
+ * This is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3, or (at your option)
+ * any later version.
+ * 
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this software; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#ifndef INCLUDED_INETS_SINK_IMPL_H
+#define INCLUDED_INETS_SINK_IMPL_H
+
+#include <inets/sink.h>
+
+#include <fstream>
+
+namespace gr {
+  namespace inets {
+
+    class sink_impl : public sink
+    {
+     private:
+         std::string filename;
+         bool overwrite;
+         std::fstream f;
+
+     public:
+      sink_impl(const char *file_name);
+      ~sink_impl();
+
+      // Where all the action really happens
+      int work(int noutput_items,
+	       gr_vector_const_void_star &input_items,
+	       gr_vector_void_star &output_items);
+    };
+
+    } // namespace inets
+} // namespace gr
+
+#endif /* INCLUDED_INETS_SINK_IMPL_H */
+
+/* -*- c++ -*- */
+/* 
+ * Copyright 2015 <+YOU OR YOUR COMPANY+>.
+ * 
+ * This is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3, or (at your option)
+ * any later version.
+ * 
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this software; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#ifndef INCLUDED_INETS_SINK_IMPL_H
+#define INCLUDED_INETS_SINK_IMPL_H
+
+#include <inets/sink.h>
+
+namespace gr {
+  namespace inets {
+
+    class sink_impl : public sink
+    {
+     private:
+      // Nothing to declare in this block.
+
+     public:
+      sink_impl();
+      ~sink_impl();
+
+      // Where all the action really happens
+      int work(int noutput_items,
+	       gr_vector_const_void_star &input_items,
+	       gr_vector_void_star &output_items);
+    };
+
+  } // namespace inets
+} // namespace gr
+
+#endif /* INCLUDED_INETS_SINK_IMPL_H */
+
--- python/__init__.py
+++ python/__init__.py
@@ -36,6 +36,7 @@ from unmake_packet_python import unmake_packet_python
 from stop_and_wait_arq import stop_and_wait_arq
 from rrrm import rrrm
 from per_logger import per_logger
+from source import source
 
 
 #
