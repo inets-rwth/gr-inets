fcfb7d8f0ff614f9088054ae3572c7888c6ac39e
Julian Arnold <julian@elitecoding.org>
2016-03-23 09:49:12 +0100

    frame_sync: bug fix
    rx_time tag was always attached to first sample in input buffer when
    found somewhere in the input buffer

--- lib/frame_sync_cc_impl.cc
+++ lib/frame_sync_cc_impl.cc
@@ -1,17 +1,17 @@
 /* -*- c++ -*- */
-/* 
+/*
  * Copyright 2015 <+YOU OR YOUR COMPANY+>.
- * 
+ *
  * This is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 3, or (at your option)
  * any later version.
- * 
+ *
  * This software is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this software; see the file COPYING.  If not, write to
  * the Free Software Foundation, Inc., 51 Franklin Street,
@@ -47,7 +47,7 @@ namespace gr {
      * The private constructor
      * Needed to make preamble len a multiple of 8.
      * Otherwise the gnuradio blocks packed_to_unpacked and unpacked_to_packed don't work
-     * beacuse the payload will not be byte aligned.        
+     * beacuse the payload will not be byte aligned.
      */
     frame_sync_cc_impl::frame_sync_cc_impl(const std::vector<int> &preamble, gr::digital::constellation_sptr constellation, float threshold, const std::string &len_tag_key)
       : gr::block("frame_sync_cc",
@@ -59,7 +59,7 @@ namespace gr {
         _len_preamble = _preamble.size();
         modulate_preamble();
         _len_preamble = _mod_preamble.size();
-        std::cout << "Using preamble of len " << _len_preamble << std::endl;
+        std::cout << "[frame_sync_cc_impl] :: Using preamble of len " << _len_preamble << std::endl;
         set_tag_propagation_policy(TPP_DONT);
         set_output_multiple(1024);
         message_port_register_out(pmt::string_to_symbol("phase"));
@@ -81,14 +81,14 @@ namespace gr {
     }
 
     void frame_sync_cc_impl::modulate_preamble()
-    {    
+    {
         _mod_preamble.clear();
         int bits_per_sym = _constellation->bits_per_symbol();
         //Use Big Endian (MSB first) to be compatible with constellation modulator block
         for(int i = 0; i < _len_preamble; i+= bits_per_sym) {
             int val = 0;
             for(int j = 0; j < bits_per_sym; j++) {
-                val += (_preamble[i + ((bits_per_sym - 1) - j)] << j); 
+                val += (_preamble[i + ((bits_per_sym - 1) - j)] << j);
             }
             _mod_preamble.push_back(_constellation->points()[val]);
         }
@@ -100,12 +100,12 @@ namespace gr {
         return;
     }
 
-    std::complex<float>* curr_correlation_buffer; // = (gr_complex*)volk_malloc(sizeof(gr_complex)*256, volk_get_alignment()); 
+    std::complex<float>* curr_correlation_buffer; // = (gr_complex*)volk_malloc(sizeof(gr_complex)*256, volk_get_alignment());
     std::complex<float>* ab =  (gr_complex*)volk_malloc(sizeof(gr_complex)*256, volk_get_alignment());
     std::complex<float>* ab_conj =(gr_complex*)volk_malloc(sizeof(gr_complex)*256, volk_get_alignment());
     std::complex<float>* cd = (gr_complex*)volk_malloc(sizeof(gr_complex)*256, volk_get_alignment());
     long rx_time = 0;
-    
+
     int
     frame_sync_cc_impl::general_work(int noutput_items,
                           gr_vector_int &ninput_items,
@@ -114,10 +114,9 @@ namespace gr {
     {
 
         boost::lock_guard<boost::mutex> guard(_set_lock);
-        
 
         const gr_complex *in = (const gr_complex *) input_items[0];
-        gr_complex *out = (gr_complex *) output_items[0];               
+        gr_complex *out = (gr_complex *) output_items[0];
         gr_complex *corr_out = (gr_complex *) output_items[1];
         unsigned char *trig_out = (unsigned char *) output_items[2];
         float *f_offset_out = (float*) output_items[3];
@@ -126,52 +125,49 @@ namespace gr {
         int num_f_offset_prod = 0;
         int consumed_items = 0;
         int produced_items = 0;
-        int i, j; 
+        int i, j;
         int preamble_items_left = 0;
         gr_complex sum = 0;
 
         //look for tx_time tag
         std::vector<tag_t> tags;
-        get_tags_in_window(tags, 0, 0, noutput_items, pmt::intern("rx_time")); 
+        get_tags_in_window(tags, 0, 0, noutput_items, pmt::intern("rx_time"));
         if(tags.size() > 0) {
             rx_time_full_sec = pmt::to_uint64(pmt::tuple_ref(tags[0].value, 0));
             rx_time_frac_sec = pmt::to_double(pmt::tuple_ref(tags[0].value, 1));
             std::cout << "received rx_time. Full Sec = " << rx_time_full_sec << " Frac Sec = " << rx_time_frac_sec << std::endl;
-            add_item_tag(0, nitems_written(0), pmt::intern("rx_time"), pmt::make_tuple(pmt::from_uint64(rx_time_full_sec), pmt::from_double(rx_time_frac_sec)));          
+            add_item_tag(0, tags[0].offset, pmt::intern("rx_time"), pmt::make_tuple(pmt::from_uint64(rx_time_full_sec), pmt::from_double(rx_time_frac_sec)));
         }
-        
+
         for(i = 0; i < noutput_items - _len_preamble; i++) {
 
             trig_out[i] = 0;
 
-	        //Look for preamble. Use differentially encoded preamble for increased detection range
+            //Look for preamble. Use differentially encoded preamble for increased detection range
             sum = 0;
-            
             //volk_32fc_x2_conjugate_dot_prod_32fc(&sum, &in[i], &_mod_preamble[0], _len_preamble);
             volk_32fc_x2_multiply_conjugate_32fc(ab, &in[i], &_mod_preamble[0], _len_preamble);
             volk_32fc_conjugate_32fc(ab_conj, ab, _len_preamble);
             volk_32fc_x2_multiply_32fc(cd, &ab[1], ab_conj, _len_preamble - 1);
             curr_correlation_buffer = ab;
-            
             //volk_32fc_x2_multiply_conjugate_32fc(cd, &_mod_buffer[0], &in[i - 1], _len_preamble);
-             
+
             for(j = 0; j < _len_preamble - 1; j++) {
-                //std::complex<float> x = std::conj(_mod_preamble[j]) * in[i + j]; 
-                //curr_correlation_buffer[j] = x; 
-                
+                //std::complex<float> x = std::conj(_mod_preamble[j]) * in[i + j];
+                //curr_correlation_buffer[j] = x;
                 //Differential detection for better freq. offset agnostic
                 //sum += x * conj(x_prev)
                 //if(j >= 1) {
                 //    sum += curr_correlation_buffer[j] * std::conj(curr_correlation_buffer[j - 1]);
                 //}
-                //sum += std::conj(in[i + j]) * in[i + j + (_len_preamble / 2)];   
+                //sum += std::conj(in[i + j]) * in[i + j + (_len_preamble / 2)];
                 //sum += std::conj(in[i + j]) * _mod_preamble[j];
                 sum += cd[j];
             }
-            
+
             corr_out[i] = sum;
-            
-            if(_state == STATE_DETECT) {                    
+
+            if(_state == STATE_DETECT) {
                 if(std::abs(sum) > _threshold) {
                     _state = STATE_PREAMBLE;
                 } else {
@@ -179,25 +175,22 @@ namespace gr {
                     produced_items++;
                 }
             }
-                                    
-            if(_state == STATE_PREAMBLE) { 
+
+            if(_state == STATE_PREAMBLE) {
                 if((noutput_items - _len_preamble - i) >= (_len_preamble)) {
                     _state = STATE_PROCESS_PREAMBLE;
-                    preamble_items_left = _len_preamble;                                            
+                    preamble_items_left = _len_preamble;
                 } else {
                     break;
                 }
             }
 
             if(_state == STATE_PROCESS_PREAMBLE) {
-                
                 if(preamble_items_left == _len_preamble) {
-                    
                     _d_f = calculate_fd(curr_correlation_buffer, &in[i], &_mod_preamble[0], _len_preamble / 2, _len_preamble);
                     //_d_phi = _mod_preamble[_len_preamble - 1] / in[i + (_len_preamble - 1)];
                     f_offset_out[num_f_offset_prod] = _d_f;
                     num_f_offset_prod++;
-                    
                     std::complex<float> curr_d_phi(0,0);
                     for(int j = 0; j < (_len_preamble / 4); j++) {
                         std::complex<float> curr_pre_item = in[j + i] * std::polar(1.0f, (float)(-2.0 * M_PI * _d_f * (float)j));
@@ -211,16 +204,16 @@ namespace gr {
                 consumed_items++;
                 produced_items++;
                 preamble_items_left--;
- 
+
                 if(preamble_items_left == 0) {
                     _state = STATE_SET_TRIGGER;
                     continue;
-                }                           
+                }
             }
- 
+
             if(_state == STATE_SET_TRIGGER) {
                 trig_out[i] = 1;
-                add_item_tag(0, nitems_written(0) + i, pmt::intern("fd"), pmt::from_float(_d_f));          
+                add_item_tag(0, nitems_written(0) + i, pmt::intern("fd"), pmt::from_float(_d_f))
                 add_item_tag(0, nitems_written(0) + i, pmt::intern("phi"), pmt::from_float(std::arg(_d_phi)));
                  _state = STATE_DETECT;
                 consumed_items++;
@@ -233,11 +226,11 @@ namespace gr {
             out[i] = in[i];
         }
 
-        consume_each(consumed_items); 
+        consume_each(consumed_items);
         produce(0, produced_items);
         produce(1, produced_items);
         produce(2, produced_items);
-        produce(3, num_f_offset_prod);                 
+        produce(3, num_f_offset_prod);
         return WORK_CALLED_PRODUCE;
 
     }
@@ -246,7 +239,7 @@ namespace gr {
     {
         while(phi > 2.0f * M_PI) {
             phi -= 2.0f * M_PI;
-        } 
+        }
         while(phi < -2.0f * M_PI) {
             phi += 2.0f * M_PI;
         }
@@ -261,17 +254,17 @@ namespace gr {
 
     /*
      * Data aided frequency offset estimation.
-     * See Eq. #8 in "Data-Aided Frequency Estimation for Burst Digital Transmission" by Mengali and Morelli 
+     * See Eq. #8 in "Data-Aided Frequency Estimation for Burst Digital Transmission" by Mengali and Morelli
      */
     float frame_sync_cc_impl::calculate_fd(const gr_complex* z, const gr_complex* x,const gr_complex* c, int N, int L0)
     {
         double w_div =(float)N * (4.0f * (float)N * (float)N - 6.0f * (float)N * (float)L0 + 3.0f * (float)L0 * (float)L0 - 1.0f);
-        //z(k) = x(k) * conj(c(k))   
+        //z(k) = x(k) * conj(c(k))
         //std::complex<double>* z = new std::complex<double>[L0];
         //for(int i = 0; i < L0; i++) {
         //    z[i] = (1 / std::abs(x[i])) * x[i] * std::conj(c[i]);
         //}
- 
+
         double sum = 0;
         for(int i = 1; i <= N; i++) {
             double w = (3.0f * ((float)(L0 - i) * (float)(L0 - i + 1) - (float)N * (float)(L0 - N))) / w_div;
@@ -283,7 +276,7 @@ namespace gr {
         }
         return ((float)sum / (2.0f * M_PI));
     }
-    
+
     std::complex<double> frame_sync_cc_impl::calculate_R(int m, const gr_complex* z, int L0)
     {
         std::complex<double> sum = 0;
@@ -292,7 +285,7 @@ namespace gr {
             std::complex<float> x2 = std::polar(1.0f, x);
             sum += x2;
         }
-        return ((1.0/(double)(L0 - m)) * sum); 
+        return ((1.0/(double)(L0 - m)) * sum);
     }
 
   } /* namespace inets */
