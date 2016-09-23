50540681e78a0471e75593087b1016ff492c55d9
inets <julian.arnold@rwth-aachen.de>
2016-09-23 10:37:41 +0200

    creating tap0 on boot

--- tools/setup_env.sh
+++ tools/setup_env.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+ip tuntap add mode tap dev tap0 user inets
+ip addr add dev tap0 192.168.200.1/24
+ip link set dev tap0 up
+
 for cpu_id in `seq 0 7`; do
     cpufreq-set -c $cpu_id -f 2.7G
 done	
